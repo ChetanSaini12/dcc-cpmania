9c24636ddceeb81d5a76f6f71d05392b31bb4372
chetansaini <chetansaini1241@gmail.com>
2023-08-17 18:47:42 +0530

    Profile Connected with Backend

--- pages/profile/[username].jsx
+++ pages/profile/[username].jsx
@@ -1,56 +1,79 @@
-import React, { useState, useEffect }from 'react'
-import { useRouter} from 'next/router'
-import { useSelector } from 'react-redux'
-
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-    const router = useRouter()
-    const { username } = router.query
-    const {loggedIn} = useSelector(state => state.login)
+  const router = useRouter();
+  const { username } = router.query;
+  const { loggedIn } = useSelector((state) => state.login);
 
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-        profile_pic: "",    
-        leetcodeURL: "",
-        atcoderURL: "",
-        codeforcesURL: "",
-        codechefURL: ""
-    });
+  const [user, setUser] = useState({
+    name: "Chetan Saini",
+    email: "chetansaini1241@gmail.com",
+    profile_pic: "https://ui-avatars.com/api/?name=Chetan Saini&size=128&background=random",
+    leetcodeUN: "chetan_saini21",
+    atcoderUN: "chetan_saini12",  
+    codeforcesUN: "chetan_saini",
+    codechefUN: "chetan_saini21",
+  });
 
-    useEffect(() => {
-        if (!loggedIn) {
-          router.push('/login');
-        } else {
-          (async () => {
-            try {
-              const response = await fetch(`http://localhost:7000/profile/${username}`, {
-                method: "GET",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              });
-      
-              if (response.ok) {
-                const data = await response.json();
-                // Handle the data here
-              } else {
-                console.error('Error fetching user data:', response.statusText);
-              }
-            } catch (error) {
-              console.error('Error fetching user data:', error);
-            }
-          })();
-        }
-      }, []);
-      
+  const [isLoading, setIsLoading] = useState(false); // Add loading state
 
+  // useEffect(() => {
+  //   if (!loggedIn) {
+  //     router.push("/login");
+  //   } else {
+  //     console.log({ username });
+  //     if (username !== undefined) {
+  //       (async () => {
+  //         try {
+  //           const response = await fetch(
+  //             `http://localhost:7000/profile/${username}`,
+  //             {
+  //               method: "GET",
+  //               headers: {
+  //                 "Content-Type": "application/json",
+  //               },
+  //             }
+  //           );
+  
+  //           if (response.ok) {
+  //             const data = await response.json();
+  //             console.log(data);
+  //             setUser(data);
+  //           } else {
+  //             console.error("Error fetching user data:", response.statusText);
+  //           }
+  //         } catch (error) {
+  //           console.error("Error fetching user data:", error);
+  //         } finally {
+  //           setIsLoading(false); // Set loading state to false after API call
+  //         }
+  //       })();
+  //     } 
+  //   }
+  // }, [loggedIn, setUser ]);
 
-    return (
-        <div id = 'blog'>
+  return (
+    <>
+      {isLoading ? ( // Render loading state while waiting for data
+        <h1>Loading...</h1>
+      ) : (
+        username !== undefined && (
+          <div id="dashboard">
             <h1>{username}</h1>
-        </div>
-    )
-}
+            <h1>{user.name}</h1>
+            <h2>{user.email}</h2>
+            <img src={user.profile_pic} alt="profile pic" />
+            <h2>{user.leetcodeUN}</h2>
+            <h2>{user.atcoderUN}</h2>
+            <h2>{user.codeforcesUN}</h2>
+            <h2>{user.codechefUN}</h2>
+          </div>
+        )
+      )}
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
